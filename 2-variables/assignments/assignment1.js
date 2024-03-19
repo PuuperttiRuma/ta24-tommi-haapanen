cc1ccce605126ebdadf2ad62d2fafe277523221c
PuuperttiRuma <thaapane@student.jyu.fi>
2024-03-19 15:52:15 +0200

    2-variables, assignment 1

---
+++ 2-variables/assignments/assignment1.js
@@ -0,0 +1,14 @@
+let name = "Tommi";
+let age = 39;
+let favProgrammingLanguage = "javascript";
+
+console.log(name);
+console.log(age);
+console.log(favProgrammingLanguage);
+
+name = "Puupertti Ruma";
+age = "Old as Time itself";
+favProgrammingLanguage = "Python"
+console.log(name);
+console.log(age);
+console.log(favProgrammingLanguage);
