af74b750381d4a7c49b0f81d785a8ef166bea04f
PuuperttiRuma <thaapane@student.jyu.fi>
2024-03-19 15:23:33 +0200

    1. Javascript basics, assignments 1, 2 and 3

---
+++ 1-javascript-basics/assignments/assignment3.js
@@ -0,0 +1,19 @@
+let value = 2;
+// console.log("value: ", value);
+
+const increase = 3;
+// console.log("increase", increase);
+
+value = value + increase;
+// console.log("value: ", value);
+// console.log("increase", increase);
+
+// const newValue = value + increase;
+// console.log("newValue: ", newValue);
+// console.log("increase", increase);
+value = value + increase;
+
+const increaseMinusOne = increase - 1;
+value = value + increaseMinusOne;
+console.log("result: " + value);
+//console.log(increaseMinusOne);
