848181f5c13e52364bd5d01167acbadb211071f2
PuuperttiRuma <thaapane@student.jyu.fi>
2024-03-21 11:32:31 +0200

    P5A4, dummydata for testing

---
+++ 5-conditionals/assignments/a4-logical-operators.js
@@ -0,0 +1,38 @@
+const games = [
+    {
+        title: "Score 5, p/t 2",
+        score: 5,
+        hoursPlayed: 40,
+        price: 20,
+    },
+    {
+        title: "Score 5, p/t 1",
+        score: 5,
+        hoursPlayed: 5,
+        price: 5,
+    },
+    {
+        title: "Score 4, p/t 2",
+        score: 4,
+        hoursPlayed: 20,
+        price: 10,
+    },
+    {
+        title: "Score 4, p/t 5",
+        score: 4,
+        hoursPlayed: 50,
+        price: 10,
+    },
+    {
+        title: "Score 3, p/t 100",
+        score: 4,
+        hoursPlayed: 100,
+        price: 1,
+    },
+    {
+        title: "Score 4, free",
+        score: 4,
+        hoursPlayed: 1,
+        price: 0,
+    },
+];
