7ea49c6abb5eba4c1daa051527356b91cdb87547
Tommi Haapanen <thaapane@student.jyu.fi>
2024-03-21 08:33:56 +0200

    Part 5 assignment 1 done

---
+++ 5-conditionals/assignments/a1-string-length-comparisons.js
@@ -0,0 +1,20 @@
+let word1 = "productivity";
+let word2 = "assignment";
+
+const capitalize = (s) => {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+if (word1.length > word2.length){
+  console.log(`${capitalize(word1)} is a longer word than ${word2}.`);
+} else {
+  console.log(`${word2} is a longer word than ${word1}.`);
+};
+
+word1 = "lol";
+
+if (word1.length > word2.length){
+  console.log(`${capitalize(word1)} is a longer word than ${word2}.`);
+} else {
+  console.log(`${word2} is a longer word than ${word1}.`);
+};
