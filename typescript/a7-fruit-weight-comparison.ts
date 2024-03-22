e14f781f0afb2015324c70dfeb27b1adc20a6bd9
Tommi Haapanen <thaapane@student.jyu.fi>
2024-03-22 10:32:41 +0200

    Typescript installed and typescript-module done

---
+++ typescript/a7-fruit-weight-comparison.ts
@@ -0,0 +1,34 @@
+const fruits = [
+    {name: "a pear", weight: 178},
+    {name: "a lemon", weight: 120},
+    {name: "an apple", weight: 90},
+    {name: "a mango", weight: 150},
+];
+
+const averageWeight = 
+    fruits.reduce((acc, fruit) => acc + fruit.weight, 0)
+    / fruits.length;
+
+/* Näyttääpä siltä, että tämä implementointitapa ei toimi Typescriptissä tällaisenään ja
+   tämän hetkisellä osaamisellani.
+   Ongelmia on kaksi:
+   1) Hedelmä-objektille attribuutin antaminen lennosta
+   2) fruits-arrayn antaminen functiokutsussa tarvitsee tyypityksen
+*/ 
+
+// fruits.forEach(fruit => {
+//     fruit.weightFromAverage = Math.abs(fruit.weight - averageWeight);
+// });
+
+// const closestToAverage = (fruits : Array<object>) => {
+//     let closest = fruits[0];
+//     fruits.forEach((fruit)  => {
+//         if (closest.weightFromAverage > fruit.weightFromAverage) {
+//             closest = fruit;
+//         }
+//     });
+//     return closest.name;
+// }
+
+// console.log(`Fruit weighing closest to weight average is ${closestToAverage(fruits)}.`);
+
---
+++ typescript/functionCalls.ts
@@ -0,0 +1,15 @@
+{
+  const numbers = [1, 2];
+  const numberAsString = "3";
+
+  //const multiplyJS = (a, b) => a*b; gives error on (a, b)
+  const multiply = (a: number, b: number): number => a * b;
+
+  console.log(
+    `Multiplying ${numbers[0]} and ${numbers[1]} together gives ${multiply(
+      numbers[0],
+      numbers[1]
+    )}`
+  );
+  //console.log(`Multiplying ${numbers[1]} and ${numberAsString} together gives ${multiply(numbers[1], numberAsString)}`); gives error on multiply(numbers[1], numberAsString)
+}
