1f8c8ca63f3ec815b86afd45f6da3dffb3e29871
Tommi Haapanen <thaapane@student.jyu.fi>
2024-03-25 16:05:17 +0200

    P8A4 done, fruit-weight to own scope

---
+++ 8-arrays/assignments/a4-deletion.ts
@@ -0,0 +1,14 @@
+{
+  const fruits = [
+    "banana",
+    "apple",
+    "grapefruit",
+    "pear",
+    "pineapple",
+    "lemon",
+  ];
+  
+  fruits.splice(fruits.indexOf('pear'), 1)
+  console.log(fruits);
+
+}
