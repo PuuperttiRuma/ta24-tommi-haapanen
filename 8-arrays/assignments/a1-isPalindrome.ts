104b7f8b1421f33cea131f4c5e30ebec26fb0fc4
Tommi Haapanen <thaapane@student.jyu.fi>
2024-03-25 15:15:42 +0200

    P8A1 done

---
+++ 8-arrays/assignments/a1-isPalindrome.ts
@@ -0,0 +1,26 @@
+const isPalindrome = (str: string) => {  
+  const strArray = str.toLowerCase().split('');
+  const invertedStringArray = [];
+  for (let i of strArray) {
+    invertedStringArray.unshift(i);
+  }
+  //console.log(strArray, invertedStringArray);
+  for (let i = 0; i < strArray.length; i++) {
+    if (strArray[i] !== invertedStringArray[i]) { return false; };    
+  }
+  return true;
+}
+
+let testword = 'word'
+console.log(`It is ${isPalindrome(testword)} that word '${testword}' is a palindrome`);
+testword = 'Anna'
+console.log(`It is ${isPalindrome(testword)} that word '${testword}' is a palindrome`);
+testword = 'SaippuaKIVIkauppias';
+console.log(`It is ${isPalindrome(testword)} that word '${testword}' is a palindrome`);
+testword = 'Atte kumiorava varo imuketta'
+console.log(`It is ${isPalindrome(testword)} that word '${testword}' is a palindrome`);
+testword = 'Attekumioravavaroimuketta'
+console.log(`It is ${isPalindrome(testword)} that word '${testword}' is a palindrome`);
+
+// isPalindrome('saippuakauppias')
+// isPalindrome('saippuakäpykauppias')
