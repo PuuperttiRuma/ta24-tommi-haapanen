7d11fedff3fe762e1d89a09bdd26a949af0e976b
Tommi Haapanen <thaapane@student.jyu.fi>
2024-03-25 13:17:16 +0200

    P7A5 done

---
+++ 7-loops/assignments/a5-sum.ts
@@ -0,0 +1,23 @@
+const calculatePyramidalNumberToSum = (targetSum: number) => {
+  let i;
+  let sum = 0;
+  for (i = 1; sum <= targetSum; i++) {    
+    sum += i;
+    //console.log("After", i, "loop, sum:", sum);
+  }
+  return i-1;
+}
+
+//Test cases (not exhaustive): 
+let targetSum = 9;
+console.log(`For pyramidal number to exceed ${targetSum}, you have to sum up to ${calculatePyramidalNumberToSum(targetSum)}.`);
+targetSum = 10;
+console.log(`For pyramidal number to exceed ${targetSum}, you have to sum up to ${calculatePyramidalNumberToSum(targetSum)}.`);
+targetSum = 11;
+console.log(`For pyramidal number to exceed ${targetSum}, you have to sum up to ${calculatePyramidalNumberToSum(targetSum)}.`);
+
+
+targetSum = 10000;
+console.log(`For pyramidal number to exceed ${targetSum}, you have to sum up to ${calculatePyramidalNumberToSum(targetSum)}.`);
+
+
