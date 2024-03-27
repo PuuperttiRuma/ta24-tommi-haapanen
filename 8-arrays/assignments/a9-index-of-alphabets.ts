92e3c010318d2e1e9647caecdcf8cfd4996dac38
Tommi Haapanen <thaapane@student.jyu.fi>
2024-03-27 11:11:03 +0200

    P8A9 done

---
+++ 8-arrays/assignments/a9-index-of-alphabets.ts
@@ -0,0 +1,42 @@
+{
+  const charIndex: Record<string, number> = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26,
+  };
+
+  const charIndexString = (string: string) => {
+    const charArray = string.split("");
+    const indexStringArray: string[] = [];
+    charArray.forEach((char: string) => {
+      indexStringArray.push(charIndex[char].toString());      
+    });
+    return indexStringArray.join('');
+  };
+
+  console.log(charIndexString("bead")); // prints "2514"
+  console.log(charIndexString("rose")); // prints "1815195"
+}
