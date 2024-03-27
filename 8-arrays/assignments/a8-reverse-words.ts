8428827635dacb38ec912284e6182b5b000d5aa9
Tommi Haapanen <thaapane@student.jyu.fi>
2024-03-27 10:29:08 +0200

    P8A8 done

--- 8-arrays/assignments/a8-reverse-words.ts
+++ 8-arrays/assignments/a8-reverse-words.ts
@@ -1,10 +1,23 @@
 {
-    const reverseWords = (sentence: string) => {
-        let words = sentence.split(" ");
-        console.log(words);        
-        
-    }
-    const sentence = "this is a short sentence";
-    const reversed = reverseWords(sentence);
-    console.log(reversed); // prints "siht si a trohs ecnetnes"
+  const reverseWords = (sentence: string) => {
+    let wordArray = sentence.split(" ");
+    let reversedWordArray: string[] = [];
+    wordArray.forEach((word) => {
+      reversedWordArray.push(reverseString(word));
+    });
+    return reversedWordArray.join(" ");
+  };
+
+  const reverseString = (string: string) => {
+    let charArray = string.split("");
+    const reversedStringArray: string[] = [];
+    charArray.forEach(char => {
+        reversedStringArray.unshift(char);
+    })
+    return reversedStringArray.join("");
+  };
+
+  const sentence = "this is a short sentence";
+  const reversed = reverseWords(sentence);
+  console.log(reversed); // prints "siht si a trohs ecnetnes"
 }
