b602363fd969f7c68a8bdb7f1ddf9d5d18a8b040
Tommi Haapanen <thaapane@student.jyu.fi>
2024-04-02 09:35:50 +0300

    P8A7 Added a another testcase

--- 8-arrays/assignments/a7-sort-array.ts
+++ 8-arrays/assignments/a7-sort-array.ts
@@ -12,6 +12,9 @@
   };
 
   const array = [4, 19, 7, 1, 9, 22, 6, 13];
+  const array2 = [-6, 2, 45, -3, 3, 22, 7, 188];
   sortNumberArray(array);
   console.log(array); // prints [ 1, 4, 6, 7, 9, 13, 19, 22 ]
+  sortNumberArray(array2);
+  console.log(array2); // prints [ -6, -3, 2, 3, 7, 22, 45, 188 ]
 }
