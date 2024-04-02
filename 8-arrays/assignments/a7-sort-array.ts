cbf1f35b526ed45963aab28c948c6deb543a8b20
Tommi Haapanen <thaapane@student.jyu.fi>
2024-04-02 12:54:26 +0300

    P8A7 element swapping with destructuring

--- 8-arrays/assignments/a7-sort-array.ts
+++ 8-arrays/assignments/a7-sort-array.ts
@@ -3,9 +3,7 @@
     for (let i = 0; i < array.length; i++) {
       for (let j = i; j < array.length; j++) {
         if (array[j] < array[i]) {
-          const temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
+          [array[i], array[j]] = [array[j], array[i]];
         }
       }
     }
