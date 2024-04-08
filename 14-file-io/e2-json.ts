e408b05a77c9e21663150ef3e33247cef424b109
PuuperttiRuma <thaapane@student.jyu.fi>
2024-04-08 13:48:11 +0300

    P14E2 done

--- 14-file-io/e2-json.ts
+++ 14-file-io/e2-json.ts
@@ -1,6 +1,14 @@
 import fs from "fs";
 
-const forecast = {
+interface Forecast {
+    day: string;
+    temperature: number;
+    cloudy: boolean;
+    sunny: boolean;
+    windy: boolean;
+}
+
+const forecast: Forecast = {
   day: "monday",
   temperature: 20,
   cloudy: true,
@@ -8,6 +16,12 @@ const forecast = {
   windy: false,
 };
 
-const data = JSON.stringify(forecast, null, 2);
+const forecastToJSON = JSON.stringify(forecast, null, 2);
+fs.writeFileSync("forecast_data.json", forecastToJSON, "utf8");
 
-fs.writeFileSync("forecast_data.json", data, "utf8");
+const data = fs.readFileSync("forecast_data.json", "utf8");
+const newForecast: Forecast = JSON.parse(data);
+newForecast.temperature = 30;
+const newForecastToJSON = JSON.stringify(newForecast, null, 2);
+fs.writeFileSync("forecast_data.json", newForecastToJSON, "utf8");
+ 
