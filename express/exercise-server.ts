a1e8983e865ea447e13e6c4a4f4f42bdf40ccbc3
PuuperttiRuma <thaapane@student.jyu.fi>
2024-04-24 09:56:16 +0300

    Express: Counter-server done

--- express/exercise-server.ts
+++ express/exercise-server.ts
@@ -3,6 +3,7 @@ import express, {Request, Response} from "express";
 const server = express();
 
 let counter = 0;
+const users: {name: string, count: number}[] = [];
 
 server.get("/", (req, res) => {
   res.send("Just saying cool things!");
@@ -13,13 +14,27 @@ server.get("/foobar", (req, res) => {
 });
 
 server.get("/counter", (req: Request, res: Response) => {
-  const number = parseInt(req.query.number);
+  const numberString = req.query.number as string | undefined;
+  const number = parseInt(numberString || "");
   if (isNaN(number)){
     counter++;
   } else {
     counter = number;
   }
   res.send(JSON.stringify(counter));
+});
+
+server.get("/counter/:name", (req: Request, res: Response) => {
+  const name = req.params.name;
+  const user = users.find(user => user.name === name);
+  if (user){
+    user.count++;
+    res.send(`${user.name} was here ${user.count} times.`);
+  } else {
+    const newUser = {name: name, count: 1};
+    users.push(newUser);
+    res.send(`${newUser.name} was here for the first time!`);
+  }  
 });
 
 server.listen(3000, () => {
