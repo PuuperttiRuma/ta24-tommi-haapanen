4e51e7bb95ee94f35998815f8f0c0dc1740c3c2c
Tommi Haapanen <thaapane@student.jyu.fi>
2024-04-25 14:37:16 +0300

    Express: POST /student added, logging body doesn't work

--- express/student-registry-middleware.ts
+++ express/student-registry-middleware.ts
@@ -8,8 +8,17 @@ const logger = (req: Request, _res: Response, next: NextFunction) =>{
   next();
 };
 
+// Validators
+const validateStudent = (req: Request, res: Response, next: NextFunction) => {
+  const {name, email} = req.body;
+  if (typeof(name) !== "string" || typeof(email) !== "string") {
+    return res.status(400).send("Missing or invalid parameters");
+  }
+  next();
+};
+
 const unknownEndpoint = (_req: Request, res: Response) => {
   res.status(404).send({error: "We can't find what you are looking for"});
 };
 
-export {logger, unknownEndpoint};
+export {logger, unknownEndpoint, validateStudent};
--- express/student-registry.ts
+++ express/student-registry.ts
@@ -1,11 +1,20 @@
 import express, { Request, Response } from "express";
-import { logger, unknownEndpoint } from "./student-registry-middleware";
+import { logger, unknownEndpoint, validateStudent } from "./student-registry-middleware";
 
 const server = express();
 const port = 3100;
 server.listen(port, () => console.log("Listening to port", port));
 
-const students = [];
+interface Student {
+  id: number,
+  name: string,
+  email: string
+}
+
+let currentNewId = 1;
+const idGenerator = () => currentNewId++;
+
+const students: Student[] = [];
 
 // Middleware declarations
 server.use(logger);
@@ -15,6 +24,17 @@ server.use(express.urlencoded({extended: false}));
 // End-points
 server.get("/students", (_req: Request, res: Response) => {
   res.send(students);
+});
+
+server.post("/student", validateStudent, (req: Request, res: Response) => {
+  const {name, email} = req.body;
+  const studentToAdd: Student = {
+    id: idGenerator(),
+    name: name,
+    email: email
+  };  
+  students.push(studentToAdd);
+  res.status(201).send();
 });
 
 // 404
