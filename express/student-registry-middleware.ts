b03dd8f171d848a65a4a537f56175a7bfc1f6e28
Tommi Haapanen <thaapane@student.jyu.fi>
2024-04-26 15:37:50 +0300

    Express: ValidateId MW adds student to req, PUT, GET refactored to use validateID. PUT bugfixes

--- express/student-registry-middleware.ts
+++ express/student-registry-middleware.ts
@@ -1,27 +1,73 @@
-import { Request, Response, NextFunction} from "express";
+import { Request, Response, NextFunction } from "express";
+import { students } from "./student-registry";
 
-const logger = (req: Request, _res: Response, next: NextFunction) =>{
-  const {method, url, body} = req;
+const logger = (req: Request, _res: Response, next: NextFunction) => {
+  const { method, url, body } = req;
   const time = new Date().toISOString();
-  let logString = `${time}: ${method} request to ${url}`;
+  let logString = `${time}: ${method} request to "${url}"`;
   if (Object.keys(body).length > 0) {
-    logString += `. BODY: ${JSON.stringify(body)}`;
+    logString += ` BODY: ${JSON.stringify(body)}`;
   }
   console.log(logString);
   next();
 };
 
 // Validators
-const validateStudent = (req: Request, res: Response, next: NextFunction) => {
-  const {name, email} = req.body;
-  if (typeof(name) !== "string" || typeof(email) !== "string") {
+const validatePostStudent = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { name, email } = req.body;
+  if (typeof name !== "string" || typeof email !== "string") {
     return res.status(400).send("Missing or invalid parameters");
   }
   next();
 };
 
-const unknownEndpoint = (_req: Request, res: Response) => {
-  res.status(404).send({error: "We can't find what you are looking for"});
+/**
+ * Checks whether the request has an ID parameter in a valid
+ * format (ie. a number). If so, find the student with that ID
+ * and adds it as a property to the Request = req.student.
+ * If no student was found, sends 404.
+ * @param req
+ * @param res
+ * @param next
+ */
+const validateId = (req: Request, res: Response, next: NextFunction) => {
+  const id = parseInt(req.params.id);
+  if (Number.isNaN(id)) {
+    res.status(400).send("Invalid ID-parameter");
+  }
+  const student = students.find((student) => student.id === id);
+  if (student === undefined) {
+    res.status(404).send("No student with that ID found");
+  }
+  req.student = student;
+  next();
 };
 
-export {logger, unknownEndpoint, validateStudent};
+const validatePutStudent = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { name, email } = req.body;
+  if (typeof name === "string" || typeof email === "string") {
+    next();
+  } else {
+    return res.status(400).send("Missing or invalid parameters");
+  }
+};
+
+const unknownEndpoint = (_req: Request, res: Response) => {
+  res.status(404).send({ error: "We can't find what you are looking for" });
+};
+
+export {
+  logger,
+  unknownEndpoint,
+  validatePostStudent,
+  validatePutStudent,
+  validateId,
+};
--- express/student-registry.ts
+++ express/student-registry.ts
@@ -1,51 +1,88 @@
 import express, { Request, Response } from "express";
-import { logger, unknownEndpoint, validateStudent } from "./student-registry-middleware";
+import {
+  logger,
+  unknownEndpoint,
+  validateId,
+  validatePostStudent,
+  validatePutStudent,
+} from "./student-registry-middleware";
+
+interface Student {
+  id: number;
+  name: string;
+  email: string;
+}
+
+declare module "express" {
+  export interface Request {
+    student?: Student;
+  }
+}
 
 const server = express();
 const port = 3100;
 server.listen(port, () => console.log("Listening to port", port));
 
-interface Student {
-  id: number,
-  name: string,
-  email: string
-}
-
 let currentNewId = 1;
 const idGenerator = () => currentNewId++;
 
-const students: Student[] = [];
+const students: Student[] = [
+  { id: 1, name: "Tupu Ankka", email: "tupu@ankkalinna.fi" },
+  { id: 2, name: "Hupu Ankka", email: "hupu@ankkalinna.fi" },
+  { id: 3, name: "Lupu Ankka", email: "lupu@ankkalinna.fi" },
+];
 
-// Middleware declarations
+//Middleware declarations
+//#region
 server.use(express.json());
-server.use(express.urlencoded({extended: false}));
+server.use(express.urlencoded({ extended: false }));
 server.use(logger);
+//#endregion
 
-// End-points
+//End-points
+//#region
 server.get("/students", (_req: Request, res: Response) => {
   res.send(students);
 });
 
-server.get("/student/:id", (req: Request, res: Response) => {
-  const id = parseInt(req.params.id);
-  const studentToSend = students.find(student => student.id === id);
-  if (studentToSend){
-    res.send(studentToSend);
-  } else {
-    res.status(404).send("Invalid ID");
-  }  
+server.get("/students/:id", validateId, (req: Request, res: Response) => {
+  res.send(req.student);
 });
 
-server.post("/student", validateStudent, (req: Request, res: Response) => {
-  const {name, email} = req.body;
-  const studentToAdd: Student = {
+server.post("/students", validatePostStudent, (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  students.push({
     id: idGenerator(),
     name: name,
-    email: email
-  };  
-  students.push(studentToAdd);
+    email: email,
+  });
   res.status(201).send();
 });
 
+server.put(
+  "/students/:id",
+  validatePutStudent,
+  validateId,
+  (req: Request, res: Response) => {
+    const newStudent = req.student as Student; // validateId checks that there is a student
+    const name = req.body.name;
+    const email = req.body.email;
+    if (name) {
+      newStudent.name = name;
+    }
+    if (email) {
+      newStudent.email = email;
+    }
+    const oldStudentIndex = students.findIndex(
+      (oldStudent) => oldStudent.id === newStudent.id
+    );
+    students.splice(oldStudentIndex, 1, newStudent);
+    res.status(201).send();
+  }
+);
+
 // 404
 server.use(unknownEndpoint);
+//#endregion
+
+export { students };
